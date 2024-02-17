ef48b9eeb04c4e66f87a945678275cf00a20bb45
Manuel Malzer <manuelmalzer11@gmail.com>
2024-02-17 13:22:53 +0100

    implement dijkstra

--- lib/algorythms/AStarAlgorithm.py
+++ lib/algorythms/AStarAlgorithm.py
@@ -1,4 +1,4 @@
-from lib.algorythms.ISearchAlgorithm import ISearchAlgorithm
+from lib.algorythms.ISearchAlgorithm import ISearchAlgorithm, numpy as np
 
 class AStarAlgorithm(ISearchAlgorithm):
     def __init__(self, name):
@@ -11,4 +11,48 @@ class AStarAlgorithm(ISearchAlgorithm):
         return ((b[0] - c[0])**2 + (b[1] - c[1])**2)**0.5
 
     def search(self, graph, start, end):
-        pass
+        # A* initialisieren
+        # Anzahl der Elemente berechnen
+        l = {}   # Pfadlängen
+        v = []   # Besuchte Punkte
+        p = {}   # Vorgänger
+        R = []   # kuerzester Pfad gefunden
+        h = {}   # Heuristik
+    
+
+        l[start]=0
+        w=start
+        R.append(start)
+        while w != end: 
+            u=graph.get_neighbors(self,start)
+            helper={}
+            for node, gewicht in u:
+                exist=False
+                for node_l,gewicht_l in l:
+                    if node==node_l:
+                        exist=True
+                if exist:
+                    if l[node] > l[w]+gewicht:
+                        l[node] = l[w]+gewicht
+                        helper[node] = l[w]+gewicht
+                        p[node]=w
+                else:
+                    l[node] = l[w]+gewicht
+                    helper[node] = l[w]+gewicht
+                    p[node]=w
+            w=min(helper, key=helper.get)
+            R.append(w)
+        
+        value=end
+        while helper_v != start:
+            
+            helper_v=p[value]
+            v.append(helper_v)
+            value=helper_v
+        
+                
+
+
+
+
+
--- lib/algorythms/DijkstraAlgorithm.py
+++ lib/algorythms/DijkstraAlgorithm.py
@@ -5,4 +5,41 @@ class DijkstraAlgorithm(ISearchAlgorithm):
         super().__init__(name)
 
     def search(self, graph, start, end):
-        pass
+         # Anzahl der Elemente berechnen
+        l = {}   # Pfadlängen
+        v = []   # Besuchte Punkte
+        p = {}   # Vorgänger
+        R = []   # kuerzester Pfad gefunden
+        h = {}   # Heuristik
+    
+
+        l[start]=0
+        w=start
+        R.append(start)
+        while w != end: 
+            u=graph.get_neighbors(self,start)
+            helper={}
+            for node, gewicht in u:
+                exist=False
+                for node_l,gewicht_l in l:
+                    if node==node_l:
+                        exist=True
+                if exist:
+                    if l[node] > l[w]+gewicht:
+                        l[node] = l[w]+gewicht
+                        helper[node] = l[w]+gewicht
+                        p[node]=w
+                else:
+                    l[node] = l[w]+gewicht
+                    helper[node] = l[w]+gewicht
+                    p[node]=w
+            w=min(helper, key=helper.get)
+            R.append(w)
+        
+        value=end
+        while helper_v != start:
+            
+            helper_v=p[value]
+            v.append(helper_v)
+            value=helper_v
+        
